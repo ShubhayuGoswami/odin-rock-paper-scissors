bd9bd84fe81d19e4ec0c499dd132824c007a087d
ShubhayuGoswami <shubhayugoswami@myyahoo.com>
2023-10-26 02:08:47 +0530

    make computerchoice function
    getComputerChoice function has been written to enable the computer to
    randomly select between rock paper and scissors.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-rock-paper-scissors
 This repo contains code for my version of the odin projects's rock paper scissors project in the foundations section.
+
+This repo only contains the JS part for now.
--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function getPlayerChoice() {
     let input = prompt('Please type your choice:');
     let playerChoice = input.toLowerCase();
-    console.log(playerChoice);
     return playerChoice;
 };
 
-getPlayerChoice();
+function getComputerChoice() {
+    let computerChoice = '';
+    let randomizer = Math.floor(Math.random()*3)+1;
+    switch (randomizer) {
+        case 1:
+            computerChoice = 'scissor';
+            break;
+        
+        case 2:
+            computerChoice = 'paper';
+            break;
+
+        case 3:
+            computerChoice = 'rock';
+            break;
+    }
+    return computerChoice;
+}
+
+console.log(getPlayerChoice());
+console.log(getComputerChoice());
