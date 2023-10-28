e1398eed123f033ab161c2cdc8edf7b864a6dc65
ShubhayuGoswami <shubhayugoswami@myyahoo.com>
2023-10-28 21:17:31 +0530

    make playround and game function
    playround function and game function have been successfully made.
    
    playround takes player choice and computer choice and then compares them
    to get the winner
    
    game function takes the number of rounds and then runs playround function
    and other helper functions that many times.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getComputerChoice() {
     let randomizer = Math.floor(Math.random()*3)+1;
     switch (randomizer) {
         case 1:
-            computerChoice = 'scissor';
+            computerChoice = 'scissors';
             break;
         
         case 2:
@@ -21,7 +21,54 @@ function getComputerChoice() {
             break;
     }
     return computerChoice;
-}
+};
 
-console.log(getPlayerChoice());
-console.log(getComputerChoice());
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+        if (playerChoice === computerChoice) {
+            console.log('Round Drawn');
+        } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+            console.log('You lose! Paper pockets Rock');
+            computerScore++;
+            return (computerScore);
+        } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+            console.log('You win! Rock bonks Scissors');0
+            playerScore++;
+            return (playerScore);
+        } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+            console.log('You win! Paper pockets Rock');
+            playerScore++;
+            return (playerScore);
+        } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+            console.log('You lose! Scissors cuts Paper');
+            computerScore++;
+            return (computerScore);
+        } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+            console.log('You win! Scissors cuts Paper');
+            playerScore++;
+            return (playerScore);
+        } else {
+            console.log('You lose! Rock bonks Scissors');
+            computerScore++
+            return (computerScore);
+        };
+};
+
+function game(number) {
+    for (let i = 1; i <= number; i++) {
+        playRound();
+    };
+
+    if (playerScore>=computerScore) {
+        console.log(`You win! You have ${playerScore} points`);
+    } else {
+        console.log(`You lose! Computer has ${computerScore} points`);
+    } 
+};
+
+game(5);
